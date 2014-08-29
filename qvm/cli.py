7ea39d77a7d3eee46382aaa19cba331f9c1285cb
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-08-29 15:53:34 +0200

    Utilities for handy management of report messaging

---
+++ qvm/cli.py
@@ -0,0 +1,11 @@
+def FATAL_ERROR(msg):
+    print '[ERROR] %s'%(msg)
+    raise SystemExit
+
+def ERROR(msg):
+    print '[ERROR] %s'%(msg)
+    raise SystemExit
+
+def WARNING(msg):
+    print '[WARNING] %s'%(msg)
+
