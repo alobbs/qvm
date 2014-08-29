4e86ca2392a95b72aaa8c94f0fe61b959743e73a
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-08-29 15:54:54 +0200

    Adds a new --os parameter for VM operating system selection

--- qvm/qvm-new.py
+++ qvm/qvm-new.py
@@ -1,5 +1,6 @@
 import os
 import cmd
+import cli
 import util
 import fedora
 import argparse
@@ -7,7 +8,7 @@ import argparse
 # Parse parameters
 parser = argparse.ArgumentParser(description='New VM')
 parser.add_argument('name',   action='store', help="VM name")
-parser.add_argument('--type', action='store', default="fedora", help="[fedora]")
+parser.add_argument('--os',   action='store', default="fedora", help="[fedora]")
 parser.add_argument('--mem',  action='store', default="1024",   help="Mb of memory")
 parser.add_argument('--cpus', action='store', default="2",      help="Number of CPUs")
 parser.add_argument('--disk', action='store', default="10G",    help="Size of the image")
@@ -18,6 +19,14 @@ vm_dir = util.get_vm_install_dir(args.name)
 vm_disk = os.path.join (vm_dir, "%s.qcow2"%(args.name))
 vm_ci_iso = os.path.join (vm_dir, "%s-cidata.iso"%(args.name))
 
+# OS
+all_vars = locals()
+all_vars.update(globals())
+if args.os in all_vars:
+    os_script = all_vars[args.os]
+else:
+    cli.FATAL_ERROR ("Unsupported OS: '%s'"%(args.os))
+
 # Clean up
 cmd.run ("rm -rf '%s'"%(vm_dir))
 cmd.run ("mkdir -p '%s'"%(vm_dir))
@@ -27,13 +36,13 @@ cmd.run ("virsh undefine %s"%(args.name))
 
 # Initial image
 if args.refresh_img:
-    fedora.download_latest_image()
+    os_script.download_latest_image()
 
-orig_img = fedora.get_latest_cached_image()
+orig_img = os_script.get_latest_cached_image()
 if not orig_img:
     # On first run there is no img
-    fedora.download_latest_image()
-    orig_img = fedora.get_latest_cached_image()
+    os_script.download_latest_image()
+    orig_img = os_script.get_latest_cached_image()
 
 cmd.run ("cp %s %s"%(orig_img, vm_disk))
 
@@ -44,8 +53,8 @@ meta_data_fp = os.path.join (vm_dir, "meta-data")
 ssh_key = open (os.path.expanduser("~/.ssh/id_rsa.pub"),'r').read()
 
 params = {'hostname':args.name, 'ssh_key': ssh_key}
-open (user_data_fp, "w+").write(fedora.USER_DATA%(params))
-open (meta_data_fp, "w+").write(fedora.META_DATA%(params))
+open (user_data_fp, "w+").write(os_script.USER_DATA%(params))
+open (meta_data_fp, "w+").write(os_script.META_DATA%(params))
 
 cmd.run ("genisoimage -output %(vm_ci_iso)s -volid cidata -joliet -r %(user_data_fp)s %(meta_data_fp)s"%(locals()))
 
@@ -71,4 +80,4 @@ cmd.run("mv %s.new %s"%(vm_disk, vm_disk))
 cmd.run ("virsh start %s"%(args.name))
 
 # Post install
-fedora.cb_post_install(args.name)
+os_script.cb_post_install(args.name)
