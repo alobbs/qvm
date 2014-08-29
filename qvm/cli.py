5741992950b8f4b4c95b6e73cdd8e0303ad3fad4
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-08-29 16:13:02 +0200

    Putting some color in my life

--- qvm/cli.py
+++ qvm/cli.py
@@ -1,11 +1,21 @@
+import sys
+
+ANSI_ERROR   = '\033[91m'
+ANSI_WARNING = '\033[93m'
+ANSI_END     = '\033[0m'
+
+def _error_pre():
+    return '[%sERROR%s]'%(ANSI_ERROR, ANSI_END)
+
+def _warning_pre():
+    return '[%sWARNING%s]'%(ANSI_WARNING, ANSI_END)
+
 def FATAL_ERROR(msg):
-    print '[ERROR] %s'%(msg)
+    ERROR(msg)
     raise SystemExit
 
 def ERROR(msg):
-    print '[ERROR] %s'%(msg)
-    raise SystemExit
+    print >> sys.stderr, '%s %s'%(_error_pre(), msg)
 
 def WARNING(msg):
-    print '[WARNING] %s'%(msg)
-
+    print >> sys.stderr, '%s %s'%(_warning_pre(), msg)
