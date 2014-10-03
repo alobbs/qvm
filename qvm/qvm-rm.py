52fc10bfdafa7aee6ff2ddb14855ec4d0ce8e2b4
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-10-03 12:05:24 +0200

    Desploy pools too

--- qvm/qvm-rm.py
+++ qvm/qvm-rm.py
@@ -7,6 +7,9 @@ host = sys.argv[1]
 
 cmd.run ("virsh destroy %s"%(host))
 cmd.run ("virsh undefine %s"%(host))
+cmd.run ("virsh pool-destroy %s"%(host))
 
 vm_dir = util.get_vm_install_dir(host)
 cmd.run ("rm -rfv '%s'"%(vm_dir))
+
+cmd.run ("virsh pool-undefine %s"%(host))
