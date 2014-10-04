27f3a2ff438780396b802c4fbaa24fb5672b2444
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-10-04 16:59:03 +0200

    Implements wait_vm_net_service()
    wait_vm_net_service() waits for a VM network service to become
    available.

--- qvm/network.py
+++ qvm/network.py
@@ -22,3 +22,45 @@ def get_image(vm_name):
     # Get image
     hds = re.findall(r"source file='(.+?)'", cont, re.I)
     return hds[0]
+
+def wait_net_service(server, port, timeout=None):
+    import errno
+    import socket
+
+    print "[INFO] Waiting for %s:%s to be available"%(server, port)
+
+    s = socket.socket()
+    if timeout:
+        from time import time as now
+        # time module is needed to calc timeout shared between two exceptions
+        end = now() + timeout
+
+    while True:
+        try:
+            if timeout:
+                next_timeout = end - now()
+                if next_timeout < 0:
+                    return False
+                else:
+            	    s.settimeout(next_timeout)
+            s.connect((server, port))
+
+        except socket.timeout, err:
+            # this exception occurs only if timeout is set
+            if timeout:
+                return False
+
+        except socket.error, err:
+            # catch timeout exception from underlying network library
+            # this one is different from socket.timeout
+            if type(err.args) != tuple or err[0] not in (errno.ETIMEDOUT, 113):
+                raise
+        else:
+            s.recv(1)
+            print "[INFO] %s:%s is available now"%(server,port)
+            s.close()
+            return True
+
+def wait_vm_net_service(vmname, port, timeout=None):
+	ip = get_ip(vmname)
+	return wait_net_service (ip, port, timeout)
