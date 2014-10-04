d1b599e8b4343bc3ae402b43899beee227a58d1e
Alvaro Lopez Ortega <alvaro@gnu.org>
2014-10-04 17:02:05 +0200

    Handier instantiation of GuestFS handlers

--- qvm/util.py
+++ qvm/util.py
@@ -15,3 +15,34 @@ def get_image_cache_dir():
 
 def get_vm_install_dir(name):
     return os.path.join (get_basedir(), 'VMs', name)
+
+def get_guestfs_vm_handler(name):
+	import guestfs
+
+	vm_dir = get_vm_install_dir(name)
+	vm_disk = os.path.join (vm_dir, "%s.qcow2"%(name))
+
+	g = guestfs.GuestFS (python_return_dict=True)
+	g.add_drive_opts (vm_disk, readonly=False)
+	g.launch()
+
+	roots = g.inspect_os()
+	assert (len(roots) > 0)
+	print roots
+
+	for root in roots:
+		# Print basic information about the operating system.
+		print "  Product name: %s" % (g.inspect_get_product_name (root))
+		print "  Version:      %d.%d" % \
+			(g.inspect_get_major_version (root),
+			 g.inspect_get_minor_version (root))
+		print "  Type:         %s" % (g.inspect_get_type (root))
+		print "  Distro:       %s" % (g.inspect_get_distro (root))
+
+		mps = g.inspect_get_mountpoints(root)
+		print "mps", mps
+		for mp_dev_target in mps:
+			print "[INFO] mount", mps[mp_dev_target], mp_dev_target
+			g.mount(mps[mp_dev_target], mp_dev_target)
+
+	return g
